d1e3f4005ef48b846e5185a9eea07a98e3030be7
Bob Clarke <bobmclarke@gmail.com>
2018-07-27 15:01:08 +0300

    Added compile.js

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -3,15 +3,9 @@ pragma solidity ^0.4.22;
 contract Inbox {
     string public message;
     
-    constructor ( bytes32[] proposalNames) public {
-    }
-
-
-    /*
     constructor (string initialMessage) public {
         message = initialMessage;
     }
-    */
 
     
     function setMessage (string newMessage) public {
