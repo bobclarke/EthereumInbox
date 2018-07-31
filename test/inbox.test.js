08424317daedd4792c9c8969113476ce9bf5742d
Bob Clarke <bobmclarke@gmail.com>
2018-07-31 11:04:34 +0300

    test

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 
-// ganache is a perosnal blockchain for development testing purposes
+// ganache is a local test network / blockchain
 const ganache = require('ganache-cli')
 
 // web3 is our NodeJS framework for interfacing with the blockchain
@@ -9,32 +9,3 @@ const Web3 = require('web3')
 
 // Instantiate an instance of Web3 and pass it the gnache provider
 const web3 = new Web3(ganache.provider());
-
-// A dummy class to demonstrate Mocha
-class Car {
-    park (){
-        return 'parked';
-    }
-
-    drive (){
-        return 'moving';
-    }
-}
-
-// Some mocha tests
-let car;
-beforeEach( function(){
-        car = new Car();
-    }
-)
-describe('Car', () =>{
-    it('can park', function () {
-        assert.equal(car.park(),'parked');
-    })
-
-    it('can drive', function () {
-        assert.equal(car.drive(), 'moving');
-    })
-});
-
-
