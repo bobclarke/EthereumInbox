00cfc6eba2566f093be05d2f6a0870930be8d09c
Bob Clarke <bobmclarke@gmail.com>
2018-07-31 17:05:08 +0300

    deploy a contract to ganache

--- compile.js
+++ compile.js
@@ -7,9 +7,9 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 // read in the solidity source code
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-//module.exports = solc.compile(source,1).contracts[':Inbox'];
+module.exports = solc.compile(source,1).contracts[':Inbox'];
 
-console.log(solc.compile(source,1).contracts[':Inbox']);
+//console.log(solc.compile(source,1).contracts[':Inbox']);
 
 
 
--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,3 +1,4 @@
+// Pull in the assert framework
 const assert = require('assert')
 
 // ganache is a local test network / blockchain
@@ -9,3 +10,39 @@ const Web3 = require('web3')
 
 // Instantiate an instance of Web3 and pass it the gnache provider
 const web3 = new Web3(ganache.provider());
+
+// get the interface and bytecode from our compile.js file
+const { interface, bytecode } = require('../compile');
+
+// Before each test case we need to use an account to deploy a contract
+let accounts, contract, contractObj;
+beforeEach( async function() {
+    // get the list of predefined gnache accounts
+    accounts = await web3.eth.getAccounts();
+
+    // The longhand way
+    /*
+    contract = new web3.eth.Contract( JSON.parse(interface) )
+    contractObj = contract.deploy({data: bytecode, arguments: ['Hi there!'] })
+    deployedContractObj = await contractObj.send({ from: accounts[0], gas:'1000000' })
+    */
+
+    // The shorthand way 
+    deployedContractObj = await new web3.eth.Contract( JSON.parse(interface) )
+        .deploy({data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas:'1000000' })
+
+
+
+
+})
+
+describe('Inbox', function () {
+    it ('deploys a contract', function(){
+        console.log(deployedContractObj);
+    });   
+});
+
+
+
+
