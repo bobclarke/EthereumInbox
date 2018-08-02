5d8378d6ea0d92b02450bfa41989cb977fb075e9
Bob Clarke <bobmclarke@gmail.com>
2018-08-02 08:48:38 +0300

    clean up

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,33 +1,34 @@
-// Pull in the assert framework
-const assert = require('assert')
 
-// ganache is a local test network / blockchain
-const ganache = require('ganache-cli')
 
-// web3 is our NodeJS framework for interfacing with the blockchain
-// Define a contructor for web3 (note the capital W)
-const Web3 = require('web3')
+/* *****************************
+ * set up
+ * *****************************/
 
-// Instantiate an instance of Web3 and pass it the gnache provider
-const web3 = new Web3(ganache.provider());
-
-// get the interface and bytecode from our compile.js file
-const { interface, bytecode } = require('../compile');
-
-// Before each test case we need to use an account to deploy a contract
+const assert = require('assert') // pull in the assert framework
+const ganache = require('ganache-cli') // ganache is a local test network / blockchain
+const Web3 = require('web3') // web3 is our NodeJS framework for interfacing with the blockchain
+const web3 = new Web3(ganache.provider()); // Instantiate an instance of Web3 and pass it the gnache provider
+const { interface, bytecode } = require('../compile'); // get the interface and bytecode from our compile.js file
 let accounts, contract, contractObj;
+
+/* *****************************
+ * pre-test initiate
+ * *****************************/
+
 beforeEach( async function() {
 
-    // get the list of predefined gnache accounts
-    accounts = await web3.eth.getAccounts();
+    accounts = await web3.eth.getAccounts();     // get the list of predefined gnache accounts
 
-    // The shorthand way 
     deployedContractObj = await new web3.eth.Contract( JSON.parse(interface) )
         .deploy({data: bytecode, arguments: ['Hi there!'] })
-        .send({ from: accounts[0], gas:'1000000' });
+        .send({ from: accounts[0], gas:'1000000' }); // deploy 
 });
 
-describe('Inbox', function () {
+/* *****************************
+ * run tests
+ * *****************************/
+
+describe('Test contract', function () {
     it ('deploys a contract', function(){
         assert.ok(deployedContractObj.options.address);
     });
@@ -41,6 +42,5 @@ describe('Inbox', function () {
         await deployedContractObj.methods.setMessage('bye!').send({ from: accounts[0] });
         const message = await deployedContractObj.methods.message().call();
         assert.equal(message, 'bye!');
-
     }); 
 });
